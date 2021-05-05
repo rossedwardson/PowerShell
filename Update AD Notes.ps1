19d6c974a77446bc0768bf953abcfb68b9e88669
Oxyarrow <redwardson@rehomelab.com>
2021-05-05 09:55:42 -0700

    Update

--- Update AD Notes.ps1
+++ Update AD Notes.ps1
@@ -1,13 +1,16 @@
 <#
 Update User Notes
 1. Import CSV with user and notes info
-2. Update users AD object
+2. Export old notes to CSV
+3. Update users AD object
 Ross Edwardson @ CMI/CORA | 05.03.2021
+Rev 1.1 - *Added Backup* 05.05.2021
 #>
 
 # Variables
-$CSVPath = ".csv"
-$LogPath = "Log.log"
+$CSVPath = "\CSV.csv"
+$LogPath = "\Log.log"
+$ExportPath = "\Backup.csv"
 
 # Start Script
 Import-Module ActiveDirectory
@@ -15,8 +18,15 @@ Import-Module ActiveDirectory
 # Start Transcript
 Start-Transcript $LogPath
 
-# Import CSV
+# Import CSV and select username for backup
 $CSV = Import-CSV -path $CSVPath -Delimiter ","
+
+# Get old notes and export to CSV for backup
+$Test = @()
+foreach ($User in $CSV) { 
+    $Test += get-aduser -Identity $($User.LoginID) -Properties Name, Info | Select-Object -Property Name, Info
+}
+$Test | Export-CSV $ExportPath -NoTypeInformation
 
 # Update Notes Object
 foreach ($row in $CSV) {
