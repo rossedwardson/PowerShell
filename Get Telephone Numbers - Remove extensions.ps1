179d5be5ef5bd586bfe85e5e05f09715157cb5a2
Ross Edwardson <redwardson@rehomelab.com>
2022-11-10 15:00:41 -0800

    Update to Rev 1.2

--- Get Telephone Numbers - Remove extensions.ps1
+++ Get Telephone Numbers - Remove extensions.ps1
@@ -3,9 +3,9 @@ Get Telephone Numbers - Remove extensions like "x***"
 1. Get All AD User's Phone Number Attributes
 2. Backup Users
 3. Create Hash of Active Users
-4. Filter out Users with $Null
-5. Filter for users with 'PAGE'
-6. Filter for users with 'X***'
+4. Filter out Users with $Null. Convert & Export.
+5. Filter for users with 'PAGE'. Convert & Export.
+6. Filter for users with 'X***'. Convert & Export.
 7. Set 'PAGE' users to $Null/clear
 8. Remove 'x' from Users
 9. Remove '.' from Users
@@ -13,7 +13,7 @@ Get Telephone Numbers - Remove extensions like "x***"
 11. Set Users Telephones
 12. Verify Change
 Ross Edwardson @ CMI/CORA | 11.10.2022
-Rev 1 | Published
+Rev 1.2 | Update
 #>
 
 # Variables
@@ -26,7 +26,9 @@ $BackupPathBefore = "*\Removal_Before.csv"
 $NPath = "*\NUsers.csv"
 $PPath = "*\PUsers.csv"
 $RPath = "*\RUsers.csv"
+$FINALFRFUsersPath = "*\FINALFRFUsers.csv"
 $BackupPathAfter = "*\Removal_After.csv"
+$FilterBackupPathBefore = "*\FilterUsersBackup.csv"
 
 # Script Start
 # Start Timer
@@ -37,9 +39,9 @@ $stopwatch.Start()
 Start-Transcript -Path $LogPath
 write-host "Starting transaction log and placing in location $LogPath"
 
-## Get Active Users
+# Get Active Users
 try {
-    $Users = @(Get-ADUser -Server $DC -Credential $Credentials -SearchBase "$SearchPath" -Properties SamAccountName, DistinguishedName, TelephoneNumber  {Enabled -eq $true})
+    $Users = @(Get-ADUser -Server $DC -Credential $Credentials -SearchBase "$SearchPath" -Properties SamAccountName, DistinguishedName, TelephoneNumber -Filter {Enabled -eq $true})
     $Users | Select-Object -Property SamAccountName, DistinguishedName, TelephoneNumber | Export-CSV -Path $BackupPathBefore -NoTypeInformation -Force
 }
 catch {
@@ -47,7 +49,7 @@ catch {
 }
 finally{
     $iCountUsers = $Users.Count
-    write-host "List of Users Generated, $iCountUsers users changed."
+    write-host "List of Users Generated, $iCountUsers users found."
 }
 
 # Create Hash
@@ -60,17 +62,34 @@ $FilterUsers = foreach ($User in $Users) {
     Write-Output (New-Object -TypeName PSObject -Property $FilterUsers)
 }
 
+# Convert HashTable to CSV and Export
+$CSVFilterUsers = $FilterUsers
+[PSCustomObject]$CSVFilterUsers | ConvertTo-CSV -NoTypeInformation -ErrorAction SilentlyContinue
+$CSVFilterUsers | Export-CSV -Path $FilterBackupPathBefore -NoTypeInformation -Force -ErrorAction SilentlyContinue
+
 # Filter out users Null Telephone
 $NUsers = $FilterUsers | where {$_.Phone -ne $Null}
-$NUsers | Export-CSV -Path $Npath -NoTypeInformation -Force
+
+# Convert HashTable to CSV and Export
+$CSVNUsers = $NUsers
+[PSCustomObject]$CSVNUsers | ConvertTo-CSV -NoTypeInformation
+$CSVNUsers | Export-CSV -Path $NPath -NoTypeInformation -Force -ErrorAction SilentlyContinue
 
 # Filter for page and export
 $PUsers = $NUsers | Where {$_.Phone -like 'PAGE'}
-$PUsers | Export-CSV -Path $PPath -NoTypeInformation -Force
+
+# Convert HashTable to CSV and Export
+$CSVPUsers = $PUsers
+[PSCustomObject]$CSVPUsers | ConvertTo-CSV -NoTypeInformation
+$CSVPUsers | Export-CSV -Path $PPath -NoTypeInformation -Force -ErrorAction SilentlyContinue
 
 # Filter x*** and export
 $RUsers = $NUsers | where {$_.Phone -like "x***"}
-$RUsers | Export-CSV -Path $RPath -NoTypeInformation -Force
+
+# Convert HashTable to CSV and Export
+$CSVRUsers = $RUsers
+[PSCustomObject]$CSVRUsers | ConvertTo-CSV -NoTypeInformation
+$CSVRUsers | Export-CSV -Path $RPath -NoTypeInformation -Force -ErrorAction SilentlyContinue
 
 # Modify RUsers Phone to match standard
 $RFUsers = @{}
@@ -123,9 +142,14 @@ catch {
     Write-Host $ErrorMessage -ForegroundColor red
 }
 
+# Output HashTables before AD Change
+$CSVFINALFRFUsers = $FINALFRFUsers
+[PSCustomObject]$CSVFINALFRFUsers | ConvertTo-CSV -NoTypeInformation -ErrorAction SilentlyContinue
+$CSVFINALFRFUsers | Export-CSV -Path $FINALFRFUsersPath -NoTypeInformation -Force -ErrorAction SilentlyContinue
+
 # Clear PageFiltered values to ADUser. Can't Set value to $Null, can only clear.
 try {
-    foreach ($PFuser in $PageFiltered) {
+    foreach ($PFuser in $PUsers) {
         Set-Aduser -Identity $PFUser.Name -Server $DC -Credential $Credentials -Clear TelephoneNumber
     }
 }
@@ -147,7 +171,7 @@ catch {
 
 # Verify all active users Telephone Change
 try {
-    $UserVerification = @(Get-ADUser -Server $DC -Credential $Credentials -SearchBase "$SearchPath" -Properties SamAccountName, DistinguishedName, TelephoneNumber  {Enabled -eq $true})
+    $UserVerification = @(Get-ADUser -Server $DC -Credential $Credentials -SearchBase "$SearchPath" -Properties SamAccountName, DistinguishedName, TelephoneNumber -Filter {Enabled -eq $true})
     $UserVerification | Select-Object -Property SamAccountName, DistinguishedName, TelephoneNumber | Export-CSV -Path $BackupPathAfter -NoTypeInformation -Force
 }
 catch {
@@ -156,7 +180,7 @@ catch {
 }
 finally{
     $iCountUsers = $Users.Count
-    write-host "List of Users Generated, $iCountUsers users changed."
+    write-host "List of Users Generated, $iCountUsers users found."
 }
 
 # Complete
